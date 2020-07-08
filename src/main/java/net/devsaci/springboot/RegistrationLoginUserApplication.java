b0bbb45afd74c09c69aca4ec6ce92f9195cc91ca
saci zakaria <sacideveloppeur@gmail.com>
2020-07-08 12:16:33 +0200

    Create and Setup Spring Boot Project in ECLIPS: Registration Login User

---
+++ src/main/java/net/devsaci/springboot/RegistrationLoginUserApplication.java
@@ -0,0 +1,13 @@
+package net.devsaci.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationLoginUserApplication.class, args);
+	}
+
+}
