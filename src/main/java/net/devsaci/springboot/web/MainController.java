8df7cd0943c803dafef076b4c9d90cb3df364df8
saci zakaria <sacideveloppeur@gmail.com>
2020-07-21 11:25:38 +0200

    Step 9 - Home PAge Implemenetation

--- src/main/java/net/devsaci/springboot/web/MainController.java
+++ src/main/java/net/devsaci/springboot/web/MainController.java
@@ -9,9 +9,13 @@ public class MainController {
 	public String login() {
 		return "login";
 	}
-
-	/*
-	 * @GetMapping("/") public String home() { return "index"; }
+	/* Step 9 - Home PAge Implemenetation 
+	 * @
 	 */
+	
+	  @GetMapping("/") 
+	  public String home() { 
+		  return "index"; }
+	 
 	
 }
